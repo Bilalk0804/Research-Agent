460d291ba6c0ebc3b81838fdd38a8db57fa455e1
Bilal Khan <141316561+Bilalk0804@users.noreply.github.com>
2025-06-17 17:52:37 +0530

    added custom functions calling in the chatbot.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_core.output_parsers import PydanticOutputParser
 from langchain.chat_models import init_chat_model
 from langchain.agents import create_tool_calling_agent,AgentExecutor
 from pydantic import BaseModel
-from tools import search_tool,wiki_tool
+from tools import search_tool,wiki_tool, save_tool
 load_dotenv()
 llm = init_chat_model("mistralai/mixtral-8x7b-instruct-v0.1", model_provider="Nvidia", api_key=os.getenv("NVIDIA_API_KEY"))
 # prompt="Tell me how is an agent diffrent from a tool in the context of LLMs"
@@ -24,21 +24,29 @@ prompt = ChatPromptTemplate.from_messages(
     [
     ("system", 
      """
-     You are a helpful research assistant that will help in generating a research paper.
-     You will provide a summary of the topic, list sources, and mention tools used in the research.
-     Wrap the output in this format and provide no other text\n{format_instructions} 
+     You are an expert research assistant powered by AI. Your job is to gather concise, high-quality insights on a given topic using custom tools (e.g., search, Wikipedia, academic databases). For each topic, follow this structure:
+
+    1. Provide a **concise summary** (150–300 words) explaining the topic clearly.
+    2. List **credible sources** used, with links if available.
+    3. Mention the **tools or methods** you used to find the information (e.g., Wikipedia wrapper, Google Search tool, academic search).
+    4. Present all content in the following format (no extra commentary):
+
+    {format_instructions}
+
+    Avoid vague responses. If you cannot find information, state clearly which part failed and suggest next steps for research.
+ 
      """),
     ("placeholder", "{chat_history}"),
     ("human", "{query}"),
     ("placeholder", "{agent_scratchpad}")
     ]).partial(format_instructions=parcer.get_format_instructions())
-tools=[search_tool,wiki_tool]
+tools=[search_tool,wiki_tool,save_tool]
 agent= create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
     tools=tools
 )
-query=input("How can I help you research?")
+query=input("How can I help you research? ")
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 raw_response = agent_executor.invoke({"query":query})
 #print(raw_response)
